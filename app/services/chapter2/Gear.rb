2bfe7c5819b5e11f86dcb35cd3e39298c67d7998
ABR <h.abr3222@gmail.com>
2020-11-23 20:20:24 +0500

    Page #20 Added the Gear Inches Functionality which Break the Older one

--- app/services/chapter2/Gear.rb
+++ app/services/chapter2/Gear.rb
@@ -1,3 +1,32 @@
+# ===========   # Page 20        ================
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog = cog
+    @rim = rim
+    @tire = tire
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    # tire goes around rim twice for diameter
+    ratio * (rim + (tire * 2))
+  end
+
+end
+
+puts Gear.new(52, 11, 26, 1.5).gear_inches
+# -> 137.090909090909
+puts Gear.new(52, 11, 24, 1.25).gear_inches
+# -> 125.272727272727
+
+=begin
+                          ===========   # Page 19        ================
 class Gear
   attr_reader :chainring, :cog
   def initialize(chainring, cog)
@@ -17,5 +46,5 @@ puts Gear.new(52, 11).ratio
 puts Gear.new(30, 27).ratio
 # -> 4.72727272727273
 # -> 1.11111111111111
-#
-#
+
+=end
